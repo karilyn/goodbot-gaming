3555c72c60334216e42facbf9c245438ac605990
Karilyn Kempton <karilyn.kempton@gmail.com>
2024-01-18 15:35:24 -0800

    Remove initial React code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Goodbot Gaming</h1>
+        <SearchBar />
       </header>
     </div>
   );
